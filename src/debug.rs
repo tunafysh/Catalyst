bca756071b42e87ea36b27ab3434418d2ad81fb6
tunafysh <hanansmani@gmail.com>
2024-09-13 10:30:23 +0200

    added more commands

--- src/debug.rs
+++ src/debug.rs
@@ -1,38 +1,88 @@
 use std::{io::{stdin, stdout, Write}, process::exit};
 use owo_colors::{OwoColorize, Stream::Stdout};
-use log::{info, warn};
+use clearscreen::clear;
+use log::{info, warn, error, debug};
+
+use crate::{lua::run_script, util::find_file};
 
 pub fn debug() {
-    
-    let _ = std::process::Command::new(if cfg!(target_family = "windows") {"cls"} else {"clear"}).status().unwrap().success();
+    clear().expect("failed to clear screen");
     info!("Debug mode enabled, dropping to Debug shell");
     loop {
         let mut cmd = String::new();
-        print!("{}", "Debug > ".if_supports_color(Stdout, |text| text.bright_purple()).bold());
+        print!("{}", "\nDebug > ".if_supports_color(Stdout, |text| text.bright_purple()).bold());
         let _ = stdout().flush();
         stdin().read_line(&mut cmd).expect("Failed to read line");
-        let cmd = cmd.trim();
-        match cmd {
+        let words: Vec<&str> = cmd.split_whitespace().collect();
+        print!("\n");
+        match words[0] {
             "exit" => {
                 warn!("Exiting");
                 exit(0);
             }
             "clr" | "clear" => {
-                let _ = std::process::Command::new(if cfg!(target_family = "windows") {"cls"} else {"clear"}).status().unwrap().success();
+                clear().expect("failed to clear screen");
                 continue;
             }
             "ping" => {
                 info!("Pong!");
+                warn!("Pong!");
+                error!("Pong!");
+                debug!("Pong!");
+                println!("Pong!");
                 continue;
+            }
+            "info" => {
+                if words.len() == 1 {
+                    println!("{}", "Missing argument".if_supports_color(Stdout, |text| text.red()).bold());
+                }
+                else {
+                    info!("{}", words[1]);
+                }
+            }
+            "warn" => {
+                if words.len() == 1 {
+                    println!("{}", "Missing argument".if_supports_color(Stdout, |text| text.red()).bold());
+                }
+                else {
+                    warn!("{}", words[1]);
+                }
+            }
+            "debug" => {
+                if words.len() == 1 {
+                    println!("{}", "Missing argument".if_supports_color(Stdout, |text| text.red()).bold());
+                }
+                else {
+                    debug!("{}", words[1]);
+                }
+            }
+            "error" => {
+                if words.len() == 1 {
+                    println!("{}", "Missing argument".if_supports_color(Stdout, |text| text.red()).bold());
+                }
+                else {
+                    error!("{}", words[1]);
+                }
+            }
+            "hook" => {
+                if words.len() == 1 {
+                    println!("{}", "Missing argument".if_supports_color(Stdout, |text| text.red()).bold());
+                }
+                else {
+                    let path = find_file(".", vec![words[1]]).unwrap();
+                    run_script(path.display().to_string()).unwrap();
+                }
             }
             "help" => {
                 println!("{}", "Commands".if_supports_color(Stdout, |text| text.purple()).underline());
                 println!("  exit");
                 println!("  ping");
                 println!("  clr, clear");
+                println!("  info, warn, error, debug <TEXT>");
+                println!("  hook <HOOK-PATH>");
                 continue;
             }
-            _ => ()
+            _ => println!("{}", "Unknown command".if_supports_color(Stdout, |text| text.red()).bold())
         }
     }
 }
